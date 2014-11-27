c88f39b77a737bf0e4eb1882c59eb6b1897734be
kevindehecker <kevindehecker@hotmail.com>
2014-11-27 14:27:40 +0100

    improved init script

--- IC/drone/initdrone.sh
+++ IC/drone/initdrone.sh
@@ -5,11 +5,14 @@ killall -9 program.elf
 mkdir -p /opt/
 #rm -f /opt/arm
 
+#install magical working dsp files!
+
 
 mkdir -p /data/video/stick
 mount /dev/sda1 /data/video/stick/
 
-ln -s /data/video/stick/opt/arm/ /opt/arm
+ln -s /data/video/stick/arm_light/ /opt/arm
+ln -s /data/video/stick/arm_light/ /opt/arm_light
 ln -s /data/video/stick/drone/ drone
 
 export PATH=/opt/arm/gst/bin:$PATH
