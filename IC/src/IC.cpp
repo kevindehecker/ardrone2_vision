cb607ace90d37dc6d53ce5278204d5b75839229d
Kevin <you@example.com>
2015-03-03 16:05:51 -0800

    oops fuck

--- IC/src/IC.cpp
+++ IC/src/IC.cpp
@@ -560,7 +560,7 @@ int main( int argc, char **argv )
    if (init(argc,argv)) {return 1;}
 
    /* clear learning buffer instead of using old stuff */
-   //textonizer.initLearner(true);
+   textonizer.initLearner(true);
 
    //initCaffe();
 
--- IC/src/textons.cpp
+++ IC/src/textons.cpp
@@ -287,8 +287,7 @@ void Textons::drawGraph(std::string msg) {
     int negative_true=0;
     int negative_false=0;
 
-    countsincelearn++; // keep track of training/test data
-	std::cout << "csll: " << countsincelearn << " \n";
+    countsincelearn++; // keep track of training/test data	
 
     int learnborder =  (lastLearnedPosition+(distribution_buf_size-distribution_buf_pointer)) % distribution_buf_size; // make a sliding graph
     if ( countsincelearn > distribution_buf_size) {
