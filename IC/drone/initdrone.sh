9a2e8e97b0d2d0ade7cbefb96c51b043201688cd
kevindehecker <kevindehecker@hotmail.com>
2015-07-13 20:21:48 +0200

    updated initdrone to install dsp files for gst framework

--- IC/drone/initdrone.sh
+++ IC/drone/initdrone.sh
@@ -1,24 +1,28 @@
-set -ex
+# install framework
 killall program.elf.respawner.sh
 killall -9 program.elf
-
-mkdir -p /opt/
-#rm -f /opt/arm
-
-#install magical working dsp files!
-
+killall -9 ap.elf
+killall -9 IC
 
 mkdir -p /data/video/stick
 mount /dev/sda1 /data/video/stick/
-
+mkdir /data/video/drone/build -p
+mkdir -p /opt/
 ln -s /data/video/stick/arm_light/ /opt/arm
 ln -s /data/video/stick/arm_light/ /opt/arm_light
 ln -s /data/video/stick/drone/ drone
-
 export PATH=/opt/arm/gst/bin:$PATH
 
+#install video frane work dsp files!
+mount /dev/sda1 /data/video/stick/
+cd /data/vide/stick/arm_full
+tar -xzf  magicalworkingdspfiles.tar.gz
+cd lib/dsp
+mkdir -p /lib/dsp
+mv * /lib/dsp/
+reboot
 
-
+#normal start:
 killall program.elf.respawner.sh
 killall -9 program.elf
 mount /dev/sda1 /data/video/stick/
@@ -28,15 +32,3 @@ cd /data/video/drone/build
 killall -9 ap.elf && ./IC
 
 
-
-
-
-
-
-
-
-
-
-
-
-/data/video/raw/ap.elf
